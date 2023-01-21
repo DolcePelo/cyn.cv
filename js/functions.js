c59e18e848b8046b2e58acea95e6175895533773
DolcePelo <104029230+DolcePelo@users.noreply.github.com>
2023-01-20 21:51:27 -0300

    Add files via upload

---
+++ js/functions.js
@@ -0,0 +1,44 @@
+const element = document.getElementById("img1");
+element.addEventListener("click", function() {
+document.getElementById("nav-img").innerHTML = "Hola yo soy Cyn!";
+document.getElementById("mapountera").style.display = "none";
+document.getElementById("nav-img").style.display = 'block'
+});
+
+const element2 = document.getElementById("img2");
+element2.addEventListener("click", function() {
+document.getElementById("nav-img").innerHTML = "Contactame 💁‍♀️ event.cyn@eventos.com";
+document.getElementById("nav-img").style.fontSize = '34px'
+document.getElementById("mapountera").style.display = "none";
+document.getElementById("nav-img").style.display = 'block'
+});
+
+const element3 = document.getElementById("img3");
+element3.addEventListener("click", function() {
+document.getElementById("nav-img").innerHTML = "Mi fecha de nac 24/09/88";
+document.getElementById("nav-img").style.fontSize = '40px'
+document.getElementById("mapountera").style.display = "none";
+document.getElementById("nav-img").style.display = 'block'
+});
+
+const element4 = document.getElementById("img4");
+element4.addEventListener("click", function() {
+document.getElementById("mapountera").style.display = "block";
+document.getElementById("nav-img").style.display = 'none'
+});
+
+const element5 = document.getElementById("img5");
+element5.addEventListener("click", function() {
+document.getElementById("nav-img").innerHTML = "Mi número +54 9 12345678";
+document.getElementById("mapountera").style.display = "none";
+document.getElementById("nav-img").style.display = 'block'
+});
+
+const element6 = document.getElementById("img6");
+element6.addEventListener("click", function() {
+document.getElementById("nav-img").innerHTML = "Organizadora de eventos, crypto trader, modelo y apasionada por la danza.";
+document.getElementById("mapountera").style.display = "none";
+document.getElementById("nav-img").style.display = 'block'
+document.getElementById("nav-img").style.fontSize = '30px'
+});
+
